451a183870e05d0fb5c747f5a23d25eb34e6c9f5
Nathan Urwin <nathan.urwin@intelligentai.co.uk>
2025-07-19 11:37:39 +0100

    add css rules

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -13,19 +13,24 @@ function App() {
         <img src={ukFlag} className="logo" alt="Union Jack Flag" />
       </div>
       <h1>UK Data Science Stats</h1>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
-      </MapContainer>
-
-      <footer>Contains public sector information licensed under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>.</footer>
+      <div className="flex-container">
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
+      <footer>
+        <div>Scanceptic</div>
+        <div>Contains public sector information licensed under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>.</div>
+        <div><a href="/">Data Sources</a></div>
+      </footer>
     </>
   );
 }
