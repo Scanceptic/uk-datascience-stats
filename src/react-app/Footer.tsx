2ed372006894dff62cd59e66f88cc150f1ba9f9b
Nathan Urwin <nathan.urwin@intelligentai.co.uk>
2025-07-26 09:28:03 +0100

    fix links

--- src/react-app/Footer.tsx
+++ src/react-app/Footer.tsx
@@ -7,8 +7,8 @@ function Footer() {
     <>
         <footer>
             <div><Link to="/about">About</Link></div>
-            <div><a href="github.com/Scanceptic">Scanceptic</a></div>
-            <div>Contains public sector information licensed under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government Licence v3.0</a>.</div>
+            <div><a href="https://www.github.com/Scanceptic" target="_blank">Scanceptic</a></div>
+            <div>Contains public sector information licensed under the <a href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/" target="_blank">Open Government Licence v3.0</a>.</div>
             <div><Link to="/datasources">Data Sources</Link></div>
         </footer>
     </>
